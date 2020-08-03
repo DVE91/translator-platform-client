6a96cbfdd69ac2bab1b41b29f01f5a90b844b5d0
DVE91 <dianevaneldik@gmail.com>
2020-08-03 17:43:22 +0200

    apiURL adapted

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
 export const defaultPaginationLimit = 10;
